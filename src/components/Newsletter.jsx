3f73a9e9de8a096dfa221bf15ad03e36047367ac
Arostegui Maranto <arosteguimaranto@outlook.com>
2023-05-13 00:53:48 -0600

    New color Nav Bar

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -11,9 +11,9 @@ export const Newsletter = () => {
                 <div className='my-4'>
                     <div className='flex flex-col sm:flex-row items-center justify-between w-full'>
                         <input className='p-3 flex w-full rounded-md text-black' type='email' placeholder='Enter Email' />
-                        <button className='bg-[#00df9a] w-[200px] rounded-md  font-medium ml-4 my-6 mx-auto py-3 text-black '>Notify me</button>
+                        <button className='bg-[##ea4171] w-[200px] rounded-md  font-medium ml-4 my-6 mx-auto py-3 text-black '>Notify me</button>
                     </div>
-                    <p> Proident et labore aliqua non reprehenderit <span className='text-[#00df9a] '> in exercitation. </span></p>
+                    <p> Proident et labore aliqua non reprehenderit <span className='text-[##ea4171] '> in exercitation. </span></p>
                 </div>
             </div>
 
