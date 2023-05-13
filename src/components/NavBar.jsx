f34c2c6de991edb334acfe2006151f7d0094148a
Arostegui Maranto <arosteguimaranto@outlook.com>
2023-05-13 13:06:55 -0600

    Nombre navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -12,7 +12,7 @@ export const NavBar = () => {
 
   return (
     <div className=' flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
-      <h1 className=' w-full text-3xl font-bold text-[##ea4171]'> ArstMrnto</h1>
+      <h1 className=' w-full text-3xl font-bold text-[##ea4171]'> Lady Gagan</h1>
       <ul className='hidden md:flex'>
         <li className='p-4'>Home</li>
         <li className='p-4'>Company</li>
