c7c328955bd49ca6d4feeef847b029039e874e5d
Arostegui Maranto <arosteguimaranto@outlook.com>
2023-05-14 18:20:09 -0600

    Album

--- src/components/Album.jsx
+++ src/components/Album.jsx
@@ -1,7 +1,6 @@
 import React from "react";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick"; 
+
 import {AlbumItem} from "./AlbumItem";
 import album from "../data/album";
 import { v4 as uuidv4 } from 'uuid';
